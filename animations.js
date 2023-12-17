273e23aa9ab509ec988de985eb0058c60c6109d7
Henry Langmack <Henry.langmack@gmail.com>
2023-12-16 23:31:12 -0500

    wave bobbing animation

---
+++ animations.js
@@ -0,0 +1,8 @@
+// setTimeout(() => {
+//     const waves = document.querySelectorAll(".wave-path");
+    
+//     waves.forEach((w) => {
+//         const randomOffset = -Math.floor(Math.random() * 100);
+//         w.style.translate = `${randomOffset}vw 0`;
+//     })
+// }, 2500)
